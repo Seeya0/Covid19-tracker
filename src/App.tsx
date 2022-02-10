48eac7c5f51d596f23ae776edeb16a5bdc7aa9aa
Seeya0 <vinlandtorfin@gmail.com>
2022-02-10 17:31:23 +0900

    App.tsxにapiを追加　エラーの修正（apiの関数のreturnが抜けていた）

--- src/App.tsx
+++ src/App.tsx
@@ -4,31 +4,30 @@ import './App.css';
 import Header from './components/Header';
 
 function App() {
-  const [data, setData] = useState({});
-  const [country, setCountry] = useState('');
+  const [data, setData] = useState<string | any>({});
+  const [country, setCountry] = useState<string>('');
 
   useEffect(() => {
     (async () => {
       const data = await fetchData()
       console.log(data)
       setData(data)
-    })
+    })();
   }, [])
 
-  const handleCountryChange = async (country) => {
+  const handleCountryChange = async (country: string) => {
     const data = await fetchData(country);
     setData(data)
     setCountry(country)
   }
 
 
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
-return (
-  <div className="App">
-    <Header />
-  </div>
-);
-}
 
 export default App;
--- src/api/index.js
+++ src/api/index.js
@@ -1,33 +1,45 @@
-import axios from "axios";
+import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async (country) => {
-    let changeableUrl = url;
+  let changeableUrl = url;
 
-    if (country) changeableUrl = `${url}/countries/${country}`
-        
-        try {
-            { data: {confirmed, recovered, deaths, lastUpdate}} = await axios.get(changeableUrl)
-        } catch (error) {
-            return error
-        }
-}
+  if (country) changeableUrl = `${url}/countries/${country}`;
+
+  try {
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(changeableUrl);
+    return { confirmed, recovered, deaths, lastUpdate };
+  } catch (error) {
+    return error;
+  }
+};
 
 export const fetchDailyData = async () => {
-    try {
-        { data } = await axios.get('https://api.covidtracking.com/v1/us/daily.json')
-        return data.map(({ positive, recovered, death, dateChecked: date }) => ({ confirmed: positive, recovered, deaths: death, date }))
-    } catch (error) {
-        return error
-    }
+  try {
+    const { data } = await axios.get(
+      'https://api.covidtracking.com/v1/us/daily.json'
+    );
+    return data.map(({ positive, recovered, death, dateChecked: date }) => ({
+      confirmed: positive,
+      recovered,
+      deaths: death,
+      date,
+    }));
+  } catch (error) {
+    return error;
+  }
 };
 
 export const fetchCountries = async () => {
-    try {
-        { data: { countries } } = await axios.get(`${url}/countries`)
-        return countries.map(country => country.name)
-    } catch (error) {
-       return error
-    }
-}
+  try {
+    const {
+      data: { countries },
+    } = await axios.get(`${url}/countries`);
+    return countries.map((country) => country.name);
+  } catch (error) {
+    return error;
+  }
+};
